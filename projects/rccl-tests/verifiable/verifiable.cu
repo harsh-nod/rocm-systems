0eb4ec7677e366f89887c8ce1e41e5c3a30dd3a2
nileshnegi <Nilesh.Negi@amd.com>
2026-02-26 19:57:06 +0000

    Increase tolerance for all_reduce_bias fp8_e5m2 on gfx950
    During verification, fp8_e5m2 reductions for all_reduce_bias are
    susceptible to rounding errors as RCCL-Tests converts to/from float32
    but RCCL converts and packs fp8_e5m2 arithmetic to/from float16 for
    gfx950.
    
    On gfx942, RCCL converts and packs fp8_e5m2 arithmetic to/from float32,
    which matches RCCL-Tests, so we do not encounter verification errors for
    all_reduce_bias

--- projects/rccl-tests/verifiable/verifiable.cu
+++ projects/rccl-tests/verifiable/verifiable.cu
@@ -1373,6 +1373,15 @@ hipError_t ncclVerifiableVerify(
   }
   #endif
 
+  // On gfx950, RCCL uses f16 intermediate for fp8_e5m2 (bfloat8) reductions
+  // while RCCL-Tests verification uses f32 intermediate
+  // allow 1 ULP rounding difference to handle validation errors
+  #if HAVE_ncclfp8_DEVICE || HAVE_ncclfp8_HOST
+  if (elt_ty == ncclFloat8e5m2) {
+    tolerance = std::max(tolerance, 1u);
+  }
+  #endif
+
   int block_n = std::min<intptr_t>(32, (elt_n + 4*512-1)/(4*512));
 
   *bad_elt_n = 0;
