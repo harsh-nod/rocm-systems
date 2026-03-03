dec089f2caf8fb1d2748ddaf48bf704c2b51986e
Mark Meserve <Mark.Meserve@amd.com>
2026-03-03 22:45:53 +0000

    revise to use synchronized objects

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk-rocattach/ptrace_runner.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk-rocattach/ptrace_runner.cpp
@@ -26,6 +26,7 @@
 
 #include "lib/common/logging.hpp"
 #include "lib/common/static_object.hpp"
+#include "lib/common/synchronized.hpp"
 
 #include <memory>
 
@@ -74,12 +75,8 @@ ptrace_runner(pid_t                       _pid,
 
 class PTraceRunner
 {
-    struct ptrace_runner_t
+    struct ptrace_runner_data_t
     {
-        // Mutex acquired by run() and held for the entire single operation
-        // Protects all variables in this struct
-        std::mutex mutex = {};
-
         // Shared data for the ptrace operation
         std::atomic<ptrace_data_t> data = {};
 
@@ -96,21 +93,25 @@ class PTraceRunner
         // Runner thread
         std::thread thread = {};
     };
-
-    std::mutex                                                m_runners_mutex;
-    std::unordered_map<int, std::shared_ptr<ptrace_runner_t>> m_runners;
+    using ptrace_runner_t = common::Synchronized<ptrace_runner_data_t>;
+    using runner_map_t =
+        common::Synchronized<std::unordered_map<int, std::shared_ptr<ptrace_runner_t>>>;
+    runner_map_t m_runners;
 
 public:
     PTraceRunner(){};
     ~PTraceRunner()
     {
-        for(auto& runner : m_runners)
-        {
-            runner.second->done.store(true);
-            runner.second->thread.join();
-            std::lock_guard map_lg(m_runners_mutex);
-            m_runners.erase(pid);
-        }
+        m_runners.wlock([&](auto& all_runners) {
+            for(auto& runner : all_runners)
+            {
+                runner.second->wlock([&](auto& data) {
+                    data.done.store(true);
+                    data.thread.join();
+                });
+            }
+            all_runners.clear();
+        });
     }
     rocattach_status_t run(pid_t              pid,
                            __ptrace_request   op,
@@ -146,98 +147,112 @@ public:
         // A shared_ptr is used so this thread can still safely wait on the mutex even if another
         // thread ends the worker thread
         std::shared_ptr<ptrace_runner_t> runner;
-        {
-            std::lock_guard map_lg(m_runners_mutex);
-            if(m_runners.count(pid) > 0)
+        m_runners.wlock([&](auto& runners) {
+            if(runners.count(pid) > 0)
             {
                 // if thread already exists, re-use it
-                runner = m_runners.at(pid);
+                runner = runners.at(pid);
             }
             else if(op == PTRACE_SEIZE)
             {
                 // if thread doesn't exist and this is a SEIZE, create it
-                runner         = std::make_shared<ptrace_runner_t>();
-                runner->thread = std::thread(ptrace_runner,
-                                             pid,
-                                             std::ref(runner->data),
-                                             std::ref(runner->running),
-                                             std::ref(runner->done));
-                m_runners.insert({pid, runner});
+                runner = std::make_shared<ptrace_runner_t>();
+                runner->wlock([&](auto& runner_data) {
+                    runner_data.thread = std::thread(ptrace_runner,
+                                                     pid,
+                                                     std::ref(runner_data.data),
+                                                     std::ref(runner_data.running),
+                                                     std::ref(runner_data.done));
+                });
+
+                runners.insert({pid, runner});
             }
             else
             {
                 // if thread doesn't exist and this isn't a SEIZE, state is incorrect
                 ROCP_ERROR
                     << "[rocprofiler-sdk-rocattach] PTraceRunner was called in an invalid state";
-                return ROCATTACH_STATUS_ERROR;
             }
-        }
+        });
 
-        std::lock_guard thread_lg(runner->mutex);
-
-        // If another thread called detach, the runner thread has been stopped and the
-        // ptrace_runner_t has been removed from PTraceRunner's maps. While the ptrace_runner_t is
-        // still valid in our shared_ptr copy, we should give up and release our reference
-        if(runner->done.load())
+        if(runner.get() == nullptr)
         {
-            ROCP_ERROR << "[rocprofiler-sdk-rocattach] ptrace was detached while another operation "
-                          "was in-flight";
             return ROCATTACH_STATUS_ERROR;
         }
 
-        // Set up the next operation
-        {
-            ptrace_data_t ptrace_data = {
-                .pid          = pid,
-                .op           = op,
-                .addr         = addr,
-                .data         = data,
-                .retval       = 0,
-                .ptrace_errno = 0,
-            };
-            runner->data.store(ptrace_data);
-        }
+        ptrace_data_t ptrace_data = {
+            .pid          = pid,
+            .op           = op,
+            .addr         = addr,
+            .data         = data,
+            .retval       = 0,
+            .ptrace_errno = 0,
+        };
 
-        // Set running to true, requesting a ptrace operation be performed in ptrace_runner() with
-        // the parameters in data
-        runner->running.store(true);
+        bool runner_error = false;
+        runner->wlock([&](auto& runner_data) {
+            // If another thread called detach, the runner thread has been stopped and the
+            // ptrace_runner_t has been removed from PTraceRunner's maps. While the ptrace_runner_t
+            // is still valid in our shared_ptr copy, we should give up and release our reference
+            if(runner_data.done.load())
+            {
+                ROCP_ERROR
+                    << "[rocprofiler-sdk-rocattach] ptrace was detached while another operation "
+                       "was in-flight";
+                runner_error = true;
+                return;
+            }
 
-        // Wait for running to be set to false, which indicates ptrace_runner() has finished the
-        // requested ptrace operation.
-        if(!wait_for_eq(runner->running, false, timeout_ms))
+            // Set up the next operation
+            runner_data.data.store(ptrace_data);
+
+            // Set running to true, requesting a ptrace operation be performed in ptrace_runner()
+            // with the parameters in data
+            runner_data.running.store(true);
+
+            // Wait for running to be set to false, which indicates ptrace_runner() has finished the
+            // requested ptrace operation.
+            if(!wait_for_eq(runner_data.running, false, timeout_ms))
+            {
+                ROCP_ERROR << "[rocprofiler-sdk-rocattach] Timeout during ptrace(" << op << ", "
+                           << pid << ", " << addr << ", " << data << ") duration: " << timeout_ms
+                           << "ms";
+                runner_error = true;
+                return;
+            }
+
+            ptrace_data = runner_data.data.load();
+
+            // If detaching, join the thread and remove its references
+            if(op == PTRACE_DETACH)
+            {
+                runner_data.done.store(true);
+                runner_data.thread.join();
+                m_runners.wlock([&](auto& runners) { runners.erase(pid); });
+            }
+        });
+
+        if(runner_error)
         {
-            ROCP_ERROR << "[rocprofiler-sdk-rocattach] Timeout during ptrace(" << op << ", " << pid
-                       << ", " << addr << ", " << data << ") duration: " << timeout_ms << "ms";
             return ROCATTACH_STATUS_ERROR;
         }
-
-        auto result = runner->data.load();
-
         if(ptrace_retval)
         {
-            *ptrace_retval = result.retval;
+            *ptrace_retval = ptrace_data.retval;
         }
         if(ptrace_errno)
         {
-            *ptrace_errno = result.ptrace_errno;
+            *ptrace_errno = ptrace_data.ptrace_errno;
         }
-        if(result.ptrace_errno != 0)
+        if(ptrace_data.ptrace_errno != 0)
         {
             // log an error if it occurs, but the ptrace call was a success, so still return success
             ROCP_ERROR << "[rocprofiler-sdk-rocattach] ptrace call failed. errno: "
-                       << result.ptrace_errno << " - " << strerror(result.ptrace_errno)
+                       << ptrace_data.ptrace_errno << " - " << strerror(ptrace_data.ptrace_errno)
                        << ". params(" << ptrace_op_name(op) << "(" << op << "), " << pid << ", "
                        << addr << ", " << data << ")";
         }
 
-        // If detaching, join the thread and remove its references
-        if(op == PTRACE_DETACH)
-        {
-            runner->done.store(true);
-            runner->thread.join();
-            std::lock_guard map_lg(m_runners_mutex);
-            m_runners.erase(pid);
-        }
         return ROCATTACH_STATUS_SUCCESS;
     }
 };
