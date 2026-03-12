32803ff23bcab897ace2d5a5eb8a699e791f364e
Gerardo Hernandez <gerardo.hernandez@amd.com>
2026-03-12 21:57:54 +0000

    ROCM-1254 - add missing projects/hip/include/hip/cooperative_groups/hip_reduce.h

---
+++ projects/hip/include/hip/cooperative_groups/hip_reduce.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/**
+ *  @file  hip_reduce.h
+ *
+ *  @brief Defines cooperative_groups reduce device API
+ */
+#ifndef HIP_INCLUDE_HIP_HIP_COOPERATIVE_GROUP_REDUCE_H
+#define HIP_INCLUDE_HIP_HIP_COOPERATIVE_GROUP_REDUCE_H
+
+#include <hip/hip_version.h>
+#include <hip/hip_common.h>
+
+#if defined(__HIP_PLATFORM_AMD__) && !defined(__HIP_PLATFORM_NVIDIA__)
+#if __cplusplus && defined(__clang__) && defined(__HIP__)
+#include <hip/amd_detail/amd_hip_cooperative_groups_reduce.h>
+#endif
+#else
+#error ("Must define exactly one of __HIP_PLATFORM_AMD__ or __HIP_PLATFORM_NVIDIA__");
+#endif
+
+#endif  // HIP_INCLUDE_HIP_HIP_COOPERATIVE_GROUP_H
